beb18baa9072ad472030f38718e43396e1920fd8
Georgi Stoyanov <33185290+geopsllc@users.noreply.github.com>
2019-05-12 15:03:37 -0500

    Add Persona v2

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ delegates = {
     'Ark':['name'],
     'DArk':['name'],
     'Qredit':['name'],
+    'Persona':['name'],
     'Phantom':['name'],
     'Ripa':['name1','name2'],
     'Kapu':['name1','name2']
@@ -23,6 +24,7 @@ nodes = {
     'Ark':'https://explorer.ark.io/api/v2',
     'DArk':'https://dexplorer.ark.io/api/v2',
     'Qredit':'https://qredit.cloud/api/v2',
+    'Persona':'https://explorer.persona.im:8443/api/v2',
     'Phantom':'https://explorer.phantom.org:8443/api/v2',
     'Ripa':'https://api.ripaex.io/api/v2',
     'Kapu':'https://api.kapunode.net/api/v2'
@@ -33,6 +35,7 @@ db = {
     'Ark':[51, 8],
     'DArk':[51, 8],
     'Qredit':[51, 8],
+    'Persona':[51, 8],
     'Phantom':[51, 8],
     'Ripa':[101, 8],
     'Kapu':[51, 8]
