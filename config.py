5d5512f68175b85b2b528d3c212cee0aa912f4b5
Thamar <33203583+ThamarD@users.noreply.github.com>
2020-09-05 23:50:00 +0200

    Added and removed DPoS network
    Added: Hydra; Compendia main and dev
    removed: Persona

--- config.py
+++ config.py
@@ -12,10 +12,12 @@ delegates = {
     'DArk':['name'],
     'Qredit':['name'],
     'Blockpool':['name1','name2'],
-    'Persona':['name1','name2'],
     'Phantom':['name1','name2'],
     'Ripa':['name1','name2'],
-    'Kapu':['name1','name2']
+    'Kapu':['name1','name2'],
+    'Hydra':['name1','name2'],
+    'Compendia-M':['name1','name2'],
+    'Compendia-D':['name1','name2']
 }
 
 # Don't change the rest unless you know what you're doing!
@@ -26,10 +28,12 @@ nodes = {
     'DArk':'https://dexplorer.ark.io/api',
     'Qredit':'https://qredit.cloud/api',
     'Blockpool':'http://api.mainnet.blockpool.io:9031/api',
-    'Persona':'https://explorer.persona.im:8443/api/v2',
     'Phantom':'https://explorer.phantom.org:8443/api/v2',
     'Ripa':'https://api.ripaex.io/api/v2',
     'Kapu':'https://api.kapunode.net/api/v2'
+    'Hydra':'http://35.195.150.223:4703/api/v2',
+    'Compendia-M':'https://api.compendia.org/api',
+    'Compendia-D':'https://api.nos.dev/api'
 }
 
 # Network Settings [delegates, blocktime]
@@ -38,8 +42,10 @@ db = {
     'DArk':[51, 8],
     'Qredit':[51, 8],
     'Blockpool':[201, 15],
-    'Persona':[51, 8],
     'Phantom':[51, 8],
     'Ripa':[101, 8],
     'Kapu':[51, 8]
+    'Hydra':[53, 8],
+    'Compendia-M':[47, 6],
+    'Compendia-D':[47, 6],
 }
