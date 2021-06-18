320001ada76fffd0dc7120cd87c6123b65d076b5
Georgi Stoyanov <33185290+geopsllc@users.noreply.github.com>
2021-06-18 17:35:30 -0500

    APIs Update

--- config.py
+++ config.py
@@ -10,36 +10,21 @@ phone = '+xxxxxxxxxxx'
 delegates = {
     'Ark':['name'],
     'DArk':['name'],
-    'Qredit':['name'],
-    'Blockpool':['name1','name2'],
-    'Persona':['name1','name2'],
-    'Phantom':['name1','name2'],
-    'Ripa':['name1','name2'],
-    'Kapu':['name1','name2']
+    'Qredit':['name1','name2']
 }
 
 # Don't change the rest unless you know what you're doing!
 
 # Network APIs
 nodes = {
-    'Ark':'https://explorer.ark.io/api',
-    'DArk':'https://dexplorer.ark.io/api',
-    'Qredit':'https://qredit.cloud/api',
-    'Blockpool':'http://api.mainnet.blockpool.io:9031/api',
-    'Persona':'https://explorer.persona.im:8443/api/v2',
-    'Phantom':'https://explorer.phantom.org:8443/api/v2',
-    'Ripa':'https://api.ripaex.io/api/v2',
-    'Kapu':'https://api.kapunode.net/api/v2'
+    'Ark':'https://api.ark.io/api',
+    'DArk':'https://dapi.ark.io/api',
+    'Qredit':'https://qredit.cloud/api'
 }
 
 # Network Settings [delegates, blocktime]
 db = {
     'Ark':[51, 8],
     'DArk':[51, 8],
-    'Qredit':[51, 8],
-    'Blockpool':[201, 15],
-    'Persona':[51, 8],
-    'Phantom':[51, 8],
-    'Ripa':[101, 8],
-    'Kapu':[51, 8]
+    'Qredit':[51, 8]
 }
